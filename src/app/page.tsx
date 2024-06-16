013b830186efa3bc222401faa9fdc2c4fa78f085
Vijay Singh <rockcodingrookie21@gmail.com>
2024-06-16 09:33:36 +0530

    acerternity_UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 
 import { AuroraBackground } from "../components/ui/aurora-background";
 
+import { LampContainer } from "../components/ui/lamp";
 
 import dynamic from "next/dynamic";
  
@@ -401,6 +402,8 @@ export default function AuroraBackgroundDemo() {
  
   return (
     <>
+
+
     <AuroraBackground>
       <motion.div
         initial={{ opacity: 0.0, y: 40 }}
@@ -454,6 +457,22 @@ export default function AuroraBackgroundDemo() {
   </div>
 </div>
 </div>
+
+
+<LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Build lamps <br /> the right way
+      </motion.h1>
+    </LampContainer>
 </>
   );
 }
