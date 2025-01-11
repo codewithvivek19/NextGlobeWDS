904d06bc14ac3a13ac84e9c0cda12c2c84080aef
codewithvivek19 <125290340+codewithvivek19@users.noreply.github.com>
2025-01-11 20:20:11 +0530

    Update page.tsx
    Positioning Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -411,12 +411,6 @@ export default function GlobeDemo() {
           }}
           className="div"
         >
-          <h2 className="text-center text-xl md:text-8xl font-bold text-black dark:text-white">
-            
-          </h2>
-            <p className="text-center text-lg md:text-xl font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
-            
-            </p>
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
         <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
